435d370fc7ab6dab13cec8db930723052e4ea4ae
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-10-26 15:22:28 +0300

    Add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="title">
+            <h2>
+                <span>/</span>Reviews
+            </h2>
+        </header>
+    );
+};
+
+export default Header;
